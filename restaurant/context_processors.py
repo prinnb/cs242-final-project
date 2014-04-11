c6167e8a9f0d8cc24586e0cfdd1304e101fea0d8
prinnb <prinn_band94@hotmail.com>
2014-04-10 22:02:42 -0500

    delete old files

--- restaurant/context_processors.py
+++
@@ -1,7 +0,0 @@
-from restaurant.models import RestaurantInfo, BusinessHours
-
-def restaurant_info(request):
-    restaurant_obj = RestaurantInfo.objects.all()[:1].get()
-    business_hours_list = BusinessHours.objects.all()
-    return {'restaurant_obj': restaurant_obj, 'business_hours_list' : business_hours_list}
-
